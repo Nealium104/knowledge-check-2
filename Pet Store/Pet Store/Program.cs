1b3dac5cc19e2f6d350a5ce87af1d09311402da9
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-12-03 09:20:09 -0500

    added the two classes

---
+++ Pet Store/Pet Store/Program.cs
@@ -0,0 +1,18 @@
+﻿using Pet_Store;
+
+Console.WriteLine("How many records do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+var recordList = new List<PlayerCharacter>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+	// In this loop, populate the object's properties using Console.ReadLine()
+	var myClass = new PlayerCharacter();
+
+	Console.WriteLine("Enter the value for ");
+	myClass.PlayerName = Console.ReadLine();
+
+	recordList.Add(myClass);
+}
+
+// Print out the list of records using Console.WriteLine()
---
+++ Pet Store/Pet Store/playerCharacter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pet_Store
+{
+    internal class PlayerCharacter : Character
+    {
+       public string PlayerName {get; set;}
+       public string PlayerClass { get; set;}
+    }
+}
